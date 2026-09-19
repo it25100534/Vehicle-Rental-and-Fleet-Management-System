e5b6172f0aebf28f6ed7cb3317c99a31d81cda84
Vilrangi W A L M <it25100558@my.sliit.lk>
2026-09-19 12:38:35 +0530

    Add vehicle models and catalogue sample data

---
+++ app/src/main/java/com/example/vehicalrentalserviceplatform/model/Suv.java
@@ -0,0 +1,42 @@
+package com.example.vehicalrentalserviceplatform.model;
+
+public class Suv extends Vehicle{
+    private int numberOfSeats;
+    private String driveTrain; //whether suv is awd 2wd or 4wd
+
+    public Suv(String make, String model, String year, double rentalRate,
+               String fuelType, double mileage, boolean isAvailable,
+               int numberOfSeats, String driveTrain, String vehicleImageFileName, String vehicleId) {
+        super(make, model, year, rentalRate, fuelType, mileage, isAvailable, vehicleImageFileName, vehicleId);
+        this.numberOfSeats = numberOfSeats;
+        setDriveTrain(driveTrain);
+    }
+
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public String getDriveTrain() {
+        return driveTrain;
+    }
+
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setDriveTrain(String driveTrain) {
+        if (driveTrain.equalsIgnoreCase("4WD")) {
+            this.driveTrain = "4WD";
+        } else if (driveTrain.equalsIgnoreCase("AWD")) {
+            this.driveTrain = "AWD";
+        } else {
+            this.driveTrain = "2WD"; // Defaults to 2WD for any case combination of Standard or any invalid input to prevent errors
+        }
+    }
+    @Override
+    public String toString() {
+        return "SUV," + super.toString() + "," + numberOfSeats + "," + driveTrain;
+    }
+}
