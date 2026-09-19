83090f7241d422adf9ad80fca197489e472a3d62
Karunatilake K K <it25100519@my.sliit.lk>
2026-09-19 13:19:33 +0530

    Add invoice and payment models

---
+++ app/src/main/java/com/example/vehicalrentalserviceplatform/billing/model/billing.java
@@ -0,0 +1,8 @@
+package com.example.vehicalrentalserviceplatform.billing.model;
+
+public class billing {
+    private Long id;
+    private double totalamount;
+    private double tax;
+    private double discount;
+}
